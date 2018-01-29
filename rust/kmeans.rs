2b3b5a062f802951c0a972a995baf61679feff24
wiffel <wiffel@tilient.net>
2018-01-29 23:33:37 +0100

    cleaned Rust version

--- rust/kmeans.rs
+++ rust/kmeans.rs
@@ -6,22 +6,24 @@ use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
 use std::hash::{Hash, Hasher};
 
-/// Main ///////////////////////////////////////////////// ///
+//--- main ---------------------------------------------------
 
 fn main() {
   let points = load_points();
   let start = Instant::now();
+
   for _ in 1 .. 30 {
     run(&points, 10, 15);
   }
   let centroids = run(&points, 10, 15);
+
   let elapsed = start.elapsed();
   let total_time = 1000.0 * elapsed.as_secs() as f64
                  + elapsed.subsec_nanos() as f64 * 1e-6;
   let iter_time = (total_time as u64) / 30;
   println!("The average time is {} ms", iter_time);
   for pt in centroids.iter() {
-    println!("{}, {}", pt.0, pt.1);
+    println!("(x: {}, y: {})", pt.0, pt.1);
   }
 }
 
@@ -37,37 +39,20 @@ fn run(points: &Points, n: usize, iters: u32) -> Points {
   centroids
 }
 
-fn load_points() -> Points {
-  let mut points = Vec::new();
-  let file = File::open("../points.txt").unwrap();
-  for line in BufReader::new(file).lines() {
-    let v = line.unwrap().split_whitespace()
-       .filter_map(|s| s.parse::<f64>().ok())
-       .collect::<Vec<_>>();
-    points.push(Point(v[0], v[1]));
-  }
-  points
-}
-
 fn clusters<'a>(xs: &'a Points,
-                centrs: &Points) -> Vec<RefPoints<'a>> {
+                centroids: &Points) -> Vec<RefPoints<'a>> {
   let mut groups: HashMap<&Point, RefPoints> = HashMap::new();
   for x in xs.iter() {
-    let y = x.closest(centrs);
+    let y = x.closest(centroids);
     match groups.entry(y) {
       Occupied(e) => { e.into_mut().push(x)  },
       Vacant(e)   => { e.insert(vec![x]); () }
     }
   }
-  groups.into_iter()
-        .map(|(_, v)| v)
-        .collect::<Vec<RefPoints>>()
+  groups.into_iter().map(|(_, v)| v).collect::<Vec<_>>()
 }
 
-/// Point //////////////////////////////////////////////// ///
-
-type Points = Vec<Point>;
-type RefPoints<'c> = Vec<&'c Point>;
+//-- Point ---------------------------------------------------
 
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
 struct Point(f64, f64);
@@ -75,9 +60,8 @@ struct Point(f64, f64);
 impl Hash for Point
 {
   fn hash<H: Hasher>(&self, state: &mut H) {
-    let Point(x, y) = *self;
-    let x: u64 = unsafe { mem::transmute(x) };
-    let y: u64 = unsafe { mem::transmute(y) };
+    let x: u64 = unsafe { mem::transmute(self.0) };
+    let y: u64 = unsafe { mem::transmute(self.1) };
     x.hash(state);
     y.hash(state);
   }
@@ -107,6 +91,11 @@ impl Point
   }
 }
 
+//-- Points --------------------------------------------------
+
+type Points = Vec<Point>;
+type RefPoints<'c> = Vec<&'c Point>;
+
 fn avg(points: &RefPoints) -> Point {
   let mut x = 0.0;
   let mut y = 0.0;
@@ -118,3 +107,16 @@ fn avg(points: &RefPoints) -> Point {
   Point(x / k, y / k)
 }
 
+fn load_points() -> Points {
+  let mut points = Vec::new();
+  let file = File::open("../points.txt").unwrap();
+  for line in BufReader::new(file).lines() {
+    let v = line.unwrap().split_whitespace()
+       .filter_map(|s| s.parse::<f64>().ok())
+       .collect::<Vec<_>>();
+    points.push(Point(v[0], v[1]));
+  }
+  points
+}
+
+//------------------------------------------------------------
